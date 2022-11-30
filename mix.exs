3f4042b4545d3f4401aca8726a30b86d0ea4e1fc
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2022-11-30 20:20:05 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ArtemProp.MixProject do
     [
       app: :absinthe_streamdata,
       consolidate_protocols: false,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
